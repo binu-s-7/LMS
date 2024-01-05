7f966e60f72e8aac92ed2c9abeef9ac0949a0919
Binath H Sooriyaarachchi <88849968+binu-s-7@users.noreply.github.com>
2024-01-06 00:00:27 +0530

    Update add-catogory.component.ts

--- API/frontend/src/app/add-catogory/add-catogory.component.ts
+++ API/frontend/src/app/add-catogory/add-catogory.component.ts
@@ -3,3 +3,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ItemsServiceService } from '../services/items-service.service';
+
+@Component({
+  selector: 'app-add-catogory',
+  templateUrl: './add-catogory.component.html',
+  styleUrls: ['./add-catogory.component.scss'],
+})
+export class AddCatogoryComponent {
+  form = new FormGroup({
+    category: new FormControl('', [Validators.required]),
+  });
