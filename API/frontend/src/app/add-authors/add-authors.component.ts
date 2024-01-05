249877a07705991cd2042b32fe64eeb598f1e967
Uvindu Chandrasiri <122516062+UvinduChandrasiri@users.noreply.github.com>
2024-01-06 00:52:54 +0530

    Update add-authors.component.ts

---
+++ API/frontend/src/app/add-authors/add-authors.component.ts
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+      <div class="col-12 col-md-8 mb-3">
+        <mat-card>
+          <mat-card-title-group>
+            <mat-card-title
+              mat-dialog-title
+              class="d-flex align-items-center mb-1"
+            >
+                 Add an Author
+            </mat-card-title>
+          </mat-card-title-group>
+<mat-divider></mat-divider>
+                
+
+
