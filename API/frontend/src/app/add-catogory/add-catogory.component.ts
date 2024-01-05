1025dc705e725045ac184e9397801158abaed503
Binath H Sooriyaarachchi <88849968+binu-s-7@users.noreply.github.com>
2024-01-05 23:50:10 +0530

    Update add-catogory.component.ts

---
+++ API/frontend/src/app/add-catogory/add-catogory.component.ts
@@ -0,0 +1,5 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ItemsServiceService } from '../services/items-service.service';
