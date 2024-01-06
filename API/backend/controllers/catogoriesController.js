b1c90307e28f39fa2bde82059e1b75809f1c3bc4
Binath H Sooriyaarachchi <88849968+binu-s-7@users.noreply.github.com>
2024-01-06 15:21:58 +0530

    Update catogoriesController.js

--- API/backend/controllers/catogoriesController.js
+++ API/backend/controllers/catogoriesController.js
@@ -11,4 +11,18 @@ const getAllCategories = asyncHandler(async (req, res) => {
 const createCategory = asyncHandler(async (req, res) => {
   const { category } = req.body;
 
+  const createdAuthor = await Category.create({
+   category,
+  });
 
+  if (createdAuthor) {
+    res.status(201).json({
+      category: createdAuthor.category,
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid data");
+  }
+});
+
+export { createCategory, getAllCategories };
