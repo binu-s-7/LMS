d549d895ffcec66e71099a4e1d911d8f21b66cae
Lakmewan Gambheera Arachchi <155669037+lakmewang@users.noreply.github.com>
2024-01-06 10:54:49 +0530

    Update itemController.js

--- API/backend/controllers/itemController.js
+++ API/backend/controllers/itemController.js
@@ -7,3 +7,40 @@ const getAllItems = asyncHandler(async (req, res) => {
   const items = await Item.find({});
   res.json(items);
 });
+
+// @desc    create new item
+// @route   POST /api/items
+const createItem = asyncHandler(async (req, res) => {
+  const { title, author, category,publisher,language, borrowedBy, status, borrowedDate } =
+    req.body;
+  const itemCount = await Item.countDocuments();
+  const item = await Item.create({
+    id: itemCount + 1,
+    title,
+    author,
+    category,
+    publisher,
+    language,
+    borrowedBy,
+    status,
+    borrowedDate,
+  });
+
+  if (item) {
+    res.status(201).json({
+      _id: item._id,
+      id: item.id,
+      title: item.title,
+      category: item.category,
+      publisher: item.publisher,
+      language: item.language,
+      author: item.author,
+      borrowedBy: item.borrowedBy,
+      status: item.status,
+      borrowedDate: item.borrowedDate,
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid data");
+  }
+});
