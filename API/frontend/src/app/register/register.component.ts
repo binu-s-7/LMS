302e7e1dc76cbb9195283216ee9b5819e7435d1a
Ariya S Subhasinghe <155760235+umeshgayan@users.noreply.github.com>
2024-01-06 14:14:40 +0530

    Update register.component.ts

--- API/frontend/src/app/register/register.component.ts
+++ API/frontend/src/app/register/register.component.ts
@@ -3,3 +3,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
