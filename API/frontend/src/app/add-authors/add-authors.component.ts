3d4ac664aa9c666bc501e366478dc0107fb6471c
Uvindu Chandrasiri <122516062+UvinduChandrasiri@users.noreply.github.com>
2024-01-06 20:10:32 +0530

    Update add-authors.component.ts

--- API/frontend/src/app/add-authors/add-authors.component.ts
+++ API/frontend/src/app/add-authors/add-authors.component.ts
@@ -4,4 +4,16 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ItemsServiceService } from '../services/items-service.service';
 
+@Component({
+  selector: 'app-add-authors',
+  templateUrl: './add-authors.component.html',
+  styleUrls: ['./add-authors.component.scss'],
+})
+
+  export class AddAuthorsComponent {
+  form = new FormGroup({
+    author: new FormControl('', [Validators.required]),
+  });
+
+
 
