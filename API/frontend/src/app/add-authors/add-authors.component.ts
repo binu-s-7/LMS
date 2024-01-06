6082c24c01f1f6a1820187e84cd95da8fa7f74a6
Uvindu Chandrasiri <122516062+UvinduChandrasiri@users.noreply.github.com>
2024-01-06 19:40:43 +0530

    Update add-authors.component.ts

--- API/frontend/src/app/add-authors/add-authors.component.ts
+++ API/frontend/src/app/add-authors/add-authors.component.ts
@@ -1,1 +1,7 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ItemsServiceService } from '../services/items-service.service';
+
 
