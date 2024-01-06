c85afe80df3823258f2241ece98ca1a8ec3e4715
Ariya S Subhasinghe <155760235+umeshgayan@users.noreply.github.com>
2024-01-06 14:13:20 +0530

    Update register.component.ts
    Import

---
+++ API/frontend/src/app/register/register.component.ts
@@ -0,0 +1,5 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
