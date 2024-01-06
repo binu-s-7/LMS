bd3f559eadff4d613e9958591b27e055aad029eb
Binath H Sooriyaarachchi <88849968+binu-s-7@users.noreply.github.com>
2024-01-06 15:16:12 +0530

    Update catogoriesController.js

--- API/backend/controllers/catogoriesController.js
+++ API/backend/controllers/catogoriesController.js
@@ -1,2 +1,12 @@
 import asyncHandler from "express-async-handler";
 import Category from "../models/catogoriesModel.js";
+
+// @route   GET /api/categories
+const getAllCategories = asyncHandler(async (req, res) => {
+  const category = await Category.find({});
+  res.json(category);
+});
+
+// @route   GET /api/categories
+const createCategory = asyncHandler(async (req, res) => {
+  const { category } = req.body;
