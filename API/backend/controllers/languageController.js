fd65d55e3dcc2b03d676cc270586ecbb32659394
Hettiarachchige Kalana Rasanjana <100579630+kalana0047@users.noreply.github.com>
2024-01-06 12:07:19 +0530

    Update languageController.js

--- API/backend/controllers/languageController.js
+++ API/backend/controllers/languageController.js
@@ -1,2 +1,8 @@
 import asyncHandler from "express-async-handler";
 import Language from "../models/languageModel.js";
+
+
+const getAllLanguages = asyncHandler(async(req,res)=>{
+  const language = await Language.find({});
+  res.json(language);
+  );
