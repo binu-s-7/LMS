3947daedd5a08506cd511e96fd823542acd22539
Binath H Sooriyaarachchi <88849968+binu-s-7@users.noreply.github.com>
2024-01-06 21:50:39 +0530

    Update view-profile.component.ts

--- API/frontend/src/app/view-profile/view-profile.component.ts
+++ API/frontend/src/app/view-profile/view-profile.component.ts
@@ -1,3 +1,21 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+@Component({
+  selector: 'app-view-profile',
+  templateUrl: './view-profile.component.html',
+  styleUrls: ['./view-profile.component.scss']
+})
+export class ViewProfileComponent {
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    nic: new FormControl('', [Validators.required]),
+  });
+
+  constructor(
+    public dialogRef: MatDialogRef<ViewProfileComponent>
+  ) {}
+
+}
