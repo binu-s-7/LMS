d1b486446d565a82d06e9ea0685a5485d126f7f2
Binath H Sooriyaarachchi <88849968+binu-s-7@users.noreply.github.com>
2024-01-06 15:11:16 +0530

    Update catogoriesController.js

--- API/backend/controllers/catogoriesController.js
+++ API/backend/controllers/catogoriesController.js
@@ -1,14 +1,1 @@
-import mongoose from 'mongoose';
 
-const categorySchema = mongoose.Schema(
-  {
-    category: {
-      type: String
-    },
-  },
-);
-
-
-const category = mongoose.model('Category', categorySchema);
-
-export default category;
