e6356cf8c957755eddd72117f8ed273a9266c2a1
Hettiarachchige Kalana Rasanjana <100579630+kalana0047@users.noreply.github.com>
2024-01-06 23:50:20 +0530

    Update languageModel.js

--- API/backend/models/languageModel.js
+++ API/backend/models/languageModel.js
@@ -1,4 +1,11 @@
 import mongoose from 'mongoose';
 const languageSchema = mongoose.Schema(
-
+  {
+    language:{
+      type:String
+    },
+  },
   );
+
+const language = mongoose.model('Language',languageSchema);
+export default language;
