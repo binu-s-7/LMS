3572a1fef06f72e4a70a1493b9561a5afed99425
Ariya S Subhasinghe <155760235+umeshgayan@users.noreply.github.com>
2024-01-06 14:33:52 +0530

    Update login.component.ts

--- API/frontend/src/app/login/login.component.ts
+++ API/frontend/src/app/login/login.component.ts
@@ -3,3 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
