a5220c8a7156c7f0fee1bc3bf830e30333e9fd8a
Lakmewan Gambheera Arachchi <155669037+lakmewang@users.noreply.github.com>
2024-01-06 18:13:25 +0530

    Update server.js

--- API/backend/server.js
+++ API/backend/server.js
@@ -14,3 +14,37 @@ import categoryRoutes from './routes/catogoriesRoutes.js'
 import cors from 'cors';
 
 const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(cors());
+app.use('/api/users', userRoutes);
+app.use('/api/items', itemRoutes);
+app.use('/api/authors', authorsRoutes);
+app.use('/api/languages', languagesRoutes);
+app.use('/api/publishers', publisherRoutes);
+app.use('/api/categories', categoryRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
