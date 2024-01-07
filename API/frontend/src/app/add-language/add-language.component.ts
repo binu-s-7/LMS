0aaa19b7bc9379e1b39ac0048d9edb6a48dbe8fc
Lakmewan Gambheera Arachchi <155669037+lakmewang@users.noreply.github.com>
2024-01-07 22:34:51 +0530

    Update add-language.component.ts

--- API/frontend/src/app/add-language/add-language.component.ts
+++ API/frontend/src/app/add-language/add-language.component.ts
@@ -1,16 +1,32 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ItemsServiceService } from '../services/items-service.service';
 
 @Component({
-  selector:'app-add-language',
-  templateUrl:'./add-language.component.html',
-  styleUrls:['./add-language.component.scss'],
+  selector: 'app-add-language',
+  templateUrl: './add-language.component.html',
+  styleUrls: ['./add-language.component.scss'],
 })
-  export class AddLanguageComponent{
+export class AddLanguageComponent {
   form = new FormGroup({
-    language:new FormControl(",[Validators.required]),
-});
+    language: new FormControl('', [Validators.required]),
+  });
 
-constructor(
-private snackBar:MatSnackBar,
-public dialogRef:MatDialogRef<AddLanguageComponent>,
-private itemsService:ItemsServiceService
-  ){}
+  constructor(
+    private snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<AddLanguageComponent>,
+    private itemsService: ItemsServiceService
+  ) {}
+
+  onSave() {
+    const language = this.form.value.language as string
+    this.itemsService.addLanguage(language).subscribe(() => {
+      this.dialogRef.close(language);
+      this.snackBar.open('Language added Successfully', 'Dismiss', {
+        duration: 3000,
+      });
+    });
+  }
+}
