de9c88ba8360a0d570a9fbb7b4688c95e5a715b2
Hettiarachchige Kalana Rasanjana <100579630+kalana0047@users.noreply.github.com>
2024-01-07 16:08:07 +0530

    Update add-language.component.ts

--- API/frontend/src/app/add-language/add-language.component.ts
+++ API/frontend/src/app/add-language/add-language.component.ts
@@ -1,4 +1,8 @@
-
+import{Componenet} from '@angular/core';
+import{FormControl, FormGroup, Validators} from'@angular/forms';
+import{MatDialogRef} from '@angular/material/dialog';
+import{MatSnackBar} from '@angular/material/snack-bar';
+import{}
 @Component({
   selector:'app-add-language',
   templateUrl:'./add-language.component.html',
