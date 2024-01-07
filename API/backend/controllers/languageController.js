718a99965201879e88d53df9d75e40b84d99ddee
Lakmewan Gambheera Arachchi <155669037+lakmewang@users.noreply.github.com>
2024-01-07 23:09:53 +0530

    Update languageController.js

--- API/backend/controllers/languageController.js
+++ API/backend/controllers/languageController.js
@@ -1,16 +1,28 @@
 import asyncHandler from "express-async-handler";
 import Language from "../models/languageModel.js";
 
-//@route GET/api/languages
-const getAllLanguages = asyncHandler(async(req,res)=>{
+// @route   GET /api/languages
+const getAllLanguages = asyncHandler(async (req, res) => {
   const language = await Language.find({});
   res.json(language);
-  );
+});
 
-// @route GET/api/languages
-const createLanguage = asyncHandler(async(req,res) =>{
-  const{ language } = req.body;
+// @route   GET /api/languages
+const createLanguage = asyncHandler(async (req, res) => {
+  const {  language  } = req.body;
 
   const createdLanguage = await Language.create({
     language,
   });
+
+  if (createdLanguage) {
+    res.status(201).json({
+      language: createdLanguage.language,
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid data");
+  }
+});
+
+export { getAllLanguages, createLanguage };
